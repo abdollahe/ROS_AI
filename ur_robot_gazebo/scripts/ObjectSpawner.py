d4141fd4c9c3fd7fd9327ac252fae9fcbc206f95
Abdollah Ebadi <abdollah.ebadi@gmail.com>
2019-05-27 08:32:33 +1000

    Working on the training data generation bug

--- ur_robot_gazebo/scripts/ObjectSpawner.py
+++ ur_robot_gazebo/scripts/ObjectSpawner.py
@@ -14,7 +14,8 @@ class ObjectSpawner:
     delete_object = None
     model_paths = []
     model_names = []
-    targetName = "" ;
+    targetName = "";
+    test_pub = None
 
     def __init__(self):
         self.model_paths.append("/home/abdollah/catkin_ws/src/skill_transfer/ur_robot/models/green_brick")
@@ -31,6 +32,8 @@ class ObjectSpawner:
         rospy.Subscriber("/ur_robot/target_obj_init_pose", PoseMessageSimple, self.spawn_callback)
         rospy.Subscriber("/ur_robot/delete_target_object", Bool, self.delete_obj_callback)
 
+        self.test_pub = rospy.Publisher("/ur_robot/delete_cube_done", Bool, queue_size=1)
+
     def spawn_callback(self, data):
         orient = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, 0))
         object_pose = Pose(Point(data.position[0], data.position[1], data.position[2]), orient)
@@ -45,10 +48,28 @@ class ObjectSpawner:
 
     def delete_obj_callback(self, data):
         print("Model name for delete is: " + self.targetName)
-        print(self.delete_object(self.targetName))
+        result = self.delete_object(self.targetName)
+        print "wait for object delete to complete"
+        if result.success:
+            print result.status_message
+            self.send_delete_cube_flag(True)
+        else:
+            print "delete of cube not successful"
+            self.send_delete_cube_flag(False)
+
+    def send_delete_cube_flag(self, value):
+        ctrl_c = False
+        while not ctrl_c:
+            print("Sending done status back")
+            if self.test_pub.get_num_connections() > 0:
+                print("Sending done status back - after checking connection")
+                msg = Bool()
+                msg.data = value
+                self.test_pub.publish(msg)
+                ctrl_c = True
 
     def randomString(self):
-        stringLength=20
+        stringLength = 20
         """Generate a random string of fixed length """
         letters = string.ascii_letters
         rand = ''.join(random.choice(letters) for i in range(stringLength))
